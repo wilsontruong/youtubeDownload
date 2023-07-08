bba793c45dc957faafbe6ba7588668f712028ca3
wilsontruong <90410232+wilsontruong@users.noreply.github.com>
2023-07-07 18:50:15 -0600

    Update ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -1,55 +1,88 @@
 import os
-from pytube import YouTube
 import time
+from tkinter import Tk, Label, Entry, Button, OptionMenu, messagebox, StringVar
+from pytube import YouTube
+
+# Function to handle download process
+
+
+def download_process():
+    url = entry.get()
+    selection = option.get()
+
+    if selection == "Video":
+        output_dir = videoDir
+        filename_prefix = 'video'
+        message = "Video is currently downloading..."
+    elif selection == "Video Clip":
+        output_dir = clipsDir
+        filename_prefix = 'video'
+        message = "Clip is currently downloading..."
+    elif selection == "Song":
+        output_dir = audioDir
+        filename_prefix = None
+        message = "Song is currently downloading..."
+    elif selection == "Sound Effect":
+        output_dir = soundDir
+        filename_prefix = None
+        message = "Sound effect is currently downloading..."
+
+    try:
+        yt = YouTube(url)
+        start_time = time.time()
+
+        if filename_prefix:
+            yt.streams.get_highest_resolution().download(
+                output_path=output_dir, filename_prefix=filename_prefix)
+        else:
+            audio = yt.streams.filter(only_audio=True).first()
+            audio.download(output_dir)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        messagebox.showinfo(
+            "Download Complete", f"This download took: {elapsed_time} seconds to complete.")
+    except Exception as e:
+        messagebox.showerror(
+            "Download Failed", f"An error occurred:\n{str(e)}")
+
 
 # Create directories for videos and audios
-videoDir = 'C:/Users/wlsn1/Desktop/Youtube Videos/Videos'
-clipsDir = 'C:/Users/wlsn1/Desktop/Youtube Videos/Clips'
-audioDir = 'C:/Users/wlsn1/Desktop/Youtube Videos/Songs'
-soundDir = 'C:/Users/wlsn1/Desktop/Youtube Videos/Sound Effects'
+# CHANGE THESE DIRECTORIES TO WHERE YOU WANT TO STORE IT! MAKE SURE IT IS A STRING!
+videoDir = ''
+clipsDir = ''
+audioDir = ''
+soundDir = ''
 
 if not os.path.exists(videoDir):
-    os.mkdir(videoDir)
+    os.makedirs(videoDir)
 if not os.path.exists(audioDir):
-    os.mkdir(audioDir)
+    os.makedirs(audioDir)
 
-selection = input(
-    "Do you want to download a song or a Youtube video?: Select 1 for videos, 2 for video clips, 3 for songs, or 4 for sound effects: ")
+# Create the main window
+window = Tk()
+window.title("YouTube Downloader")
+window.geometry("400x200")
+window.resizable(False, False)
 
-while (selection != '1' and selection != '2' and selection != '3' and selection != '4'):
-    selection = input(
-        "Invalid number, pick 1 for videos, 2 for video clips, 3 for songs, or 4 for sound effects: ")
+# Create the input label and entry field
+label_url = Label(window, text="Enter YouTube Link:")
+label_url.pack(pady=10)
+entry = Entry(window, width=50)
+entry.pack(pady=5)
 
-url = input("Enter Your Link: ")
-# create a YouTube object2
-yt = YouTube(url)
+# Create the selection label and option menu
+label_option = Label(window, text="Select Download Type:")
+label_option.pack()
+option = StringVar(window)
+option.set("Video")  # Default value is set to 'Video'
+menu = OptionMenu(window, option, "Video",
+                  "Video Clip", "Song", "Sound Effect")
+menu.pack(pady=5)
 
-start_time = time.time()
+# Create the download button
+button = Button(window, text="Download", command=download_process)
+button.pack(pady=10)
 
-if selection == '1':
-    # get the video and audio stream and download them
-    print("Video is currently downloading...")
-    yt.streams.get_highest_resolution().download(
-        output_path=videoDir, filename_prefix='video')
-
-if selection == '2':
-    # get the video and audio stream and download them
-    print("Clip is currently downloading...")
-    yt.streams.get_highest_resolution().download(
-        output_path=clipsDir, filename_prefix='video')
-
-elif selection == '3':
-    # get the videos stream and download it
-    print("Song is currently downloading...")
-    audio = yt.streams.filter(only_audio=True).first()
-    audio.download(audioDir)
-
-elif selection == '4':
-    # get the videos stream and download it
-    print("Sound effect is currently downloading...")
-    soundEffect = yt.streams.filter(only_audio=True).first()
-    soundEffect.download(soundDir)
-
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"This download took: {elapsed_time} seconds to download.")
+# Run the GUI main loop
+window.mainloop()
